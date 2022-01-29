79fc109fac44f04a128159389067f364ed5f4dcf
Annie Huang <annie.hn@gmail.com>
2022-01-29 20:47:35 +1100

    Rename TotalItems to TotalCompleteItems

--- app/src/App.js
+++ app/src/App.js
@@ -2,17 +2,17 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddTodoForm from './components/AddTodoForm';
 import TodoList from './components/TodoList';
-import TotalItems from './components/TotalItems';
+import TotalCompleteItems from './components/TotalCompleteItems';
 
 const App = () => {
-	return (
-		<div className='container bg-white p-4 mt-5'>
-			<h1>My Todo List</h1>
-			<AddTodoForm />
-			<TodoList />
-			<TotalItems />
-		</div>
-	);
+  return (
+    <div className='container bg-white p-4 mt-5'>
+      <h1>My Todo List</h1>
+      <AddTodoForm />
+      <TodoList />
+      <TotalCompleteItems />
+    </div>
+  );
 };
 
 export default App;
