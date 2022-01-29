b16983e3ceff04d25d14ad6530e47d4e86435da8
Annie Huang <annie.hn@gmail.com>
2022-01-29 20:28:25 +1100

    Add toggleComplete into reducers of the todoSlice and export its action.

--- app/src/redux/todoSlice.js
+++ app/src/redux/todoSlice.js
@@ -3,22 +3,26 @@ import { createSlice } from '@reduxjs/toolkit';
 const todoSlice = createSlice({
   name: 'todos',
   initialState: [
-    { id: 1, title: 'todo1', complete: false },
-    { id: 2, title: 'todo2', complete: false },
-    { id: 3, title: 'todo3', complete: true },
+    { id: 1, title: 'todo1', completed: false },
+    { id: 2, title: 'todo2', completed: false },
+    { id: 3, title: 'todo3', completed: true },
   ],
   reducers: {
     addTodo: (state, action) => {
       const newToDo = {
         id: Date.now(),
         title: action.payload.title,
-        complete: false,
+        completed: false,
       };
       state.push(newToDo);
+    },
+    toggleComplete: (state, action) => {
+      const index = state.findIndex((todo) => todo.id === action.payload.id);
+      state[index].completed = action.payload.completed;
     },
   },
 });
 
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, toggleComplete } = todoSlice.actions;
 
 export default todoSlice.reducer;
