a504cf3f4b1df30b4d0b16d74a84d897632a2c33
Annie Huang <annie.hn@gmail.com>
2022-01-29 19:02:42 +1100

    Fixed console problem in the dev tool

--- app/src/components/TodoItem.js
+++ app/src/components/TodoItem.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
 const TodoItem = ({ id, title, completed }) => {
-	return (
-		<li className='list-group-item'>
-			<div className='d-flex justify-content-between'>
-				<span className='d-flex align-items-center'>
-					<input type='checkbox' className='mr-3' checked={completed}></input>
-					{title}
-				</span>
-				<button className='btn btn-danger'>Delete</button>
-			</div>
-		</li>
-	);
+  const onChange = () => {};
+
+  return (
+    <li className='list-group-item'>
+      <div className='d-flex justify-content-between'>
+        <span className='d-flex align-items-center'>
+          <input
+            type='checkbox'
+            className='mr-3'
+            checked={completed}
+            onChange={onChange}
+          ></input>
+          {title}
+        </span>
+        <button className='btn btn-danger'>Delete</button>
+      </div>
+    </li>
+  );
 };
 
 export default TodoItem;
